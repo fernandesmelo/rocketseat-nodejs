0d5d82d3f08860872c371b85577ccd2533c49c47
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-03-19 14:52:33 -0300

    adiciona rotas GET e POST para gerenciamento de usuários no servidor

--- Node.js/01-fundamentos-nodejs/src/server.js
+++ Node.js/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,26 @@
 import http from 'node:http';
 
+const users = []
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Laércio Fernandes',
+      email: 'laerciofernandesmelonetoo@gmail.com'
+    });
+
+    return res.end('Usuário criado comsucesso!');
+  }
+
   return res.end('EU VOU VENCER!');
 });
 
